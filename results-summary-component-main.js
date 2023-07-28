c5756c109dd9556159922920b5d13f086c5b50c0
cmb347827 <v38d422ll621dem@gmail.com>
2023-07-28 15:49:42 -0400

    fetch json file from github raw

--- results-summary-component-main.js
+++ results-summary-component-main.js
@@ -1,7 +1,5 @@
 'use strict'; 
 
-//import { data } from "https://file.io/CFTJnN9CnQv1";
-
 const colors ={
 	'Almost White': 'hsl(0, 0%, 98%)',
 	'Lighter Gray': 'hsl(11, 2%, 95%)',
@@ -29,17 +27,19 @@ function replaceHtml(el, html) {
 };
 
 function getJson() {
-    return fetch('https://corsproxy.io/?https://www.jsonkeeper.com/b/R41L')
+    return fetch('https://raw.githubusercontent.com/cmb347827/results-summary-component-main/main/data.json')
      .then(response => response.json())
-     .then(response => {return response;})
+     .then(response => {console.log(response);return response;})
      .catch(err => {
        console.error(err);
      });  
 }
 
 async function displayData(){
+	
 	const results= document.querySelector('.results');
 	const data= await(getJson());
+	
 	let array= []; let string='';
 	data.forEach((element,index) => {
 		//empty alt for aria as are decorative img
@@ -50,20 +50,8 @@ async function displayData(){
 	$('.results').append(string);
 }
 
-/*function displayData(){
-	let array= []; let string='';
-	data.forEach((element,index) => {
-		//empty alt for aria as are decorative img
-	    string+=`<p><img class='icon' alt='' src=${element.icon}> ${element.category}  <span class='right-dark-font${index}'>${element.score}<span class='inherit-position light-font'>/100</span></span></p>`;
-    });
-	//replaceHtml('.results', string)
-	//results.innerHTML = array.join(' ');
-	$('.results').append(string);
-}*/
-
 
 $(window).on('load',function(){
-	
 	displayData();
 	const summary= document.querySelector('#summary');
 	summary.textContent = `Summary`;
